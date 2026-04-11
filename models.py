6b7511a707413560342590b0ddf20e91cf99fb82
zevaryx <zevaryx@gmail.com>
2026-04-11 21:27:00 +0000

    feat: Add /ingest endpoint for collecting wardrive data

--- models.py
+++ models.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field
 
 class WebhookRequest(BaseModel):
     event: str
     region: str
-    timestamp: int
+    timestamp: datetime
     message: str
     data: dict[str, str | int | list[str]]
+    
+class IngestRequest(BaseModel):
+    data: list[dict] = Field(default_factory=list)
